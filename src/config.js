053a630420792c485786557d87b97dd63553a36f
Lucian Lutas <lucian.lutas@saltandpepper.co>
2022-06-23 12:35:46 +0300

    Added pubSub and sendinblue services

--- src/config.js
+++ src/config.js
@@ -1,3 +1,8 @@
 export const config = {
     mongoDB: process.env.MONGO_DB,
+    sendinblue: {
+        key: process.env.SENDINBLUE_KEY,
+        fromEmail: process.env.SENDINBLUE_FROM_EMAIL,
+        fromName: process.env.SENDINBLUE_FROM_NAME,
+    },
 };
